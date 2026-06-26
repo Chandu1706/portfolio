24ceae8507cae61157e250a79fa1c5570c7ae3d6
Chandu1706 <korubillichandu123@gmail.com>
2026-06-26 07:27:25 -0600

    apply fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,6 @@ import logo from "../assets/Ck .png";
 import { FaLinkedin} from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
-import { PiLinktreeLogo } from "react-icons/pi";
-import { BsYoutube } from "react-icons/bs";
 const Navbar = () => {
   return (
 
@@ -15,17 +13,11 @@ const Navbar = () => {
   <a href="https://www.linkedin.com/in/chandu-korubilli-50597918b/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
     <FaLinkedin />
   </a>
-  <a href="https://linktr.ee/Chandu_korubilli" target="_blank" rel="noopener noreferrer" className="hover:text-green-500">
-    <PiLinktreeLogo />
-  </a>
   <a href="https://github.com/Chandu1706" target="_blank" rel="noopener noreferrer" className="hover:text-gray-500">
     <FaGithub />
   </a>
   <a href="https://www.instagram.com/chandu_korubilli17/?igsh=MXR5N2E4eGdrNmNpaQ%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
     <FaInstagram />
-  </a>
-  <a href="https://www.youtube.com/@ChanduKorubilli" target="_blank" rel="noopener noreferrer" className="hover:text-green-500">
-    <  BsYoutube/>
   </a>
 </div>
 
